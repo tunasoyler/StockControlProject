67e81ac72ff87f7ee07c153b8ebc30d4dda71691
Tuna Soyler <tunasoyler@gmail.com>
2023-04-26 16:01:42 +0300

    .

--- StockControlProject.Api/Controllers/OrderController.cs
+++ StockControlProject.Api/Controllers/OrderController.cs
@@ -10,16 +10,37 @@ namespace StockControlProject.Api.Controllers
     public class OrderController : ControllerBase
     {
         private readonly IGenericService<Order> orderService;
+        private readonly IGenericService<OrderDetails> orderDetailsService;
 
-        public OrderController(IGenericService<Order> orderService)
+        public OrderController(IGenericService<Order> orderService, IGenericService<OrderDetails> orderDetailsService)
         {
             this.orderService = orderService;
+            this.orderDetailsService = orderDetailsService;
         }
 
         [HttpGet]
         public IActionResult GetAllOrders()
         {
-            return Ok(orderService.GetAll(x=>x.User, x=>x.OrderDetails));
+            return Ok(orderService.GetAll(x => x.User, x => x.OrderDetails));
         }
+
+        [HttpGet]
+        public IActionResult GetActiveOrders()
+        {
+            return Ok(orderService.GetActive());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(int id)
+        {
+            return Ok(orderService.GetById(id, x => x.OrderDetails));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderDetailsById(int id)
+        {
+            return Ok(orderDetailsService.GetAll(x => x.OrderId == id, x => x.Product));
+        }
+
     }
 }
